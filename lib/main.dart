f722d8ad1a7ed82c1b65ea2ce2709a67b1f84adb
Andre Kho <andreyonathan65@gmail.com>
2020-01-28 15:30:16 +0700

    Module 8.20

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,18 @@ import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      // create: (ctx) => Products(), 
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: Products()),
+        ChangeNotifierProvider.value(value: Cart()),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
