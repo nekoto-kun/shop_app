765c4d816f5283dae5f69c3ed526b7b3894aaf17
Andre Kho <andreyonathan65@gmail.com>
2020-02-10 15:52:53 +0700

    Module 10.17

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -122,7 +122,19 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url = 'https://flutter-udemy-9d225.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    http.delete(url).then((response) {
+      if (response.statusCode >= 400) {
+        
+      }
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 
