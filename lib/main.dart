b6ed48efe3033f3e04de30cb340ea8279f99b289
Andre Kho <andreyonathan65@gmail.com>
2020-02-10 09:49:24 +0700

    Module 9.6

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import './screens/cart_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -6,6 +6,8 @@ import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
+import './edit_product_screen.dart';
+
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
@@ -19,7 +21,7 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => Navigator.of(context).pushNamed(EditProductScreen.routeName),
           ),
         ],
       ),
