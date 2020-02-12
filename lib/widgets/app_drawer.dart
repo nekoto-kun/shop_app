aeb94995030dba1e8ee33dd4c0ce941425aa34ba
Andre Kho <andreyonathan65@gmail.com>
2020-02-12 09:16:51 +0700

    Module 11.14

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/auth.dart';
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
 
@@ -15,21 +17,29 @@ class AppDrawer extends StatelessWidget {
           ),
           Divider(),
           ListTile(
-            leading: Icon(Icons.shop),
-            title: Text('Shop'),
-            onTap: () => Navigator.of(context).pushReplacementNamed('/')
-          ),
+              leading: Icon(Icons.shop),
+              title: Text('Shop'),
+              onTap: () => Navigator.of(context).pushReplacementNamed('/')),
           Divider(),
           ListTile(
-            leading: Icon(Icons.payment),
-            title: Text('Orders'),
-            onTap: () => Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName)
-          ),
+              leading: Icon(Icons.payment),
+              title: Text('Orders'),
+              onTap: () => Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName)),
           Divider(),
           ListTile(
-            leading: Icon(Icons.edit),
-            title: Text('Manage Products'),
-            onTap: () => Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName)
+              leading: Icon(Icons.edit),
+              title: Text('Manage Products'),
+              onTap: () => Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName)),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logOut();
+            },
           ),
         ],
       ),
